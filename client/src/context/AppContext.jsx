e21b65b598543db67fd6c1290676320b757a3f4f
rajkishork05 <kothiyalrajkishor1@gmail.com>
2025-08-19 23:22:30 +0530

    Context and values created

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,12 +1,18 @@
-import { createContext } from "react";
+import { createContext, use, useState } from "react";
 
 export const AppContext = createContext()
 
 export const AppContextProvider =(props)=>{
-   
+
+    const [searchFilter, setSearchFilter] = useState({
+        title:"",
+        location: ""
+    })
+   const [isSearch, setIsSearch] = useState(false)
    
     const value = {
-
+        setSearchFilter, searchFilter,
+        isSearch, setIsSearch,
     }
     return(<AppContext.Provider value={value} >
         {props.children}
