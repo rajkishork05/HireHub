bfab27fd87d1610acd0ffa6ad91d5d5f59a76b2d
rajkishork05 <kothiyalrajkishor1@gmail.com>
2025-08-19 23:24:47 +0530

    exportation of dummy data

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,5 +1,49 @@
  import logo from "./logo.png"
+import searchIcon from "./search_icon.svg"
+import location_icon from "./location_icon.svg"
+import samsung_logo from "./samsung_logo.png"
+import accenture_logo from "./accenture_logo.png"
+import adobe_logo from "./adobe_logo.png"
+import amazon_logo from "./amazon_logo.png"
+import microsoft_logo from "./microsoft_logo.svg"
+import walmart from "./walmart_logo.svg"
+import cross_icon from "./cross_icon.svg"
 
  export const assets ={
     logo,
+    searchIcon,
+    location_icon,
+    samsung_logo,
+    adobe_logo,
+    accenture_logo,
+    amazon_logo,
+    microsoft_logo,
+    walmart,
+    cross_icon
  }
+
+ export const JobCategories = [
+   "Programming",
+   "Data Science",
+   "Designation",
+   "Networking",
+   "Management",
+   "Marketing",
+   "Cybersecurity"
+ ]
+
+ export const JobLocation = [
+  "Bengaluru",
+  "Hyderabad",
+  "Pune",
+  "Mumbai",
+  "Delhi",
+  "Chennai",
+  "Kolkata",
+  "Gurugram",
+  "Noida",
+  "Ahmedabad",
+
+  
+];
+ 
