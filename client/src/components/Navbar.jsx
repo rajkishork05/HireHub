99b6df69cfbbfd53e0684dd7b8bce2587f551b4e
rajkishork05 <kothiyalrajkishor1@gmail.com>
2025-08-19 23:20:36 +0530

    fixed issue with clerk

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
+
+  const {openSignIn} = useClerk()
+
+  const {user} = useUser()
+
+
   return (
     <div className='shadow py-1'>
-      <div className='container py-4 2xl:px-20 mx-auto flex justify-between items-center' >
-        <img className='w-30 bg-amber-500' src={assets.logo} alt="" />
+      <div className='container py-1 2xl:px-20 mx-auto flex justify-between items-center' >
+        <img className='w-20' src={assets.logo} alt="" />
+        {
+          user 
+          ? 
+          <div className='flex item-center gap-3' >
+            <Link to={"/applications"} >Applied Jobs </Link>
+            <p></p>
+            <p>Hi, {user.firstName+" "+user.lastName}</p>
+            <UserButton/>
+          </div>
+          :
         <div className='flex gap-4 max-sm:text-xs' >
         <button className='text-gray-500' >Recruiter Login</button>
-        <button className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full' >Login</button>
-      </div>
+        <button onClick={e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full' >Login</button>
+      </div>}
       </div>
     </div>
   )
