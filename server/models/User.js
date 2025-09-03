8d2e5dfd68e09d69e02129831c0e5ed61345b3bd
rajkishork05 <kothiyalrajkishor1@gmail.com>
2025-09-03 10:30:08 +0530

    fixed userSchema

--- server/models/User.js
+++ server/models/User.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
     _id : {type:String, required:true},
     name : {type:String, required:true},
     email : {type:String, required:true, unique: true},
-    resume : {type:String, required:true},
-    image : {type:String, required:true},
+    resume : {type:String},
+    image : {type:String, required:true}
     
 })
 const User = mongoose.model("user", userSchema)
