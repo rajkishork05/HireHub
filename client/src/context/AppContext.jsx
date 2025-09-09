14690d075a34937d824cff7f8df4dac25f4988aa
rajkishork05 <kothiyalrajkishor1@gmail.com>
2025-09-09 11:27:34 +0530

    created some new context to fetch data in frontend

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,16 @@
-import { createContext, use, useEffect, useState } from "react";
-import { jobsData } from "../assets/assets";
+import { createContext, useEffect, useState } from "react";
+
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useAuth, useUser } from "@clerk/clerk-react";
 
 export const AppContext = createContext()
 
 export const AppContextProvider =(props)=>{
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const {user} = useUser()
+    const {getToken} = useAuth()
 
     const [searchFilter, setSearchFilter] = useState({
         title:"",
@@ -13,22 +20,120 @@ export const AppContextProvider =(props)=>{
 
    const [jobs, setJobs ] = useState([])
 
+    const[userData, setUserData] = useState(null)
+    const[userApplications, setUserApplications] = useState([])
+
    const[showRecruiterLogin, setShowRecruiterLogin] = useState(false)
    //fetch job data
+   const [companyToken, setCompanyToken] = useState(null)
+   const [companyData, setCompanyData] = useState(null)
+
    const fetchJobs = async()=>{
-    setJobs(jobsData)
+    try {
+        const {data} = await axios.get(backendUrl + "/api/jobs")
+        if(data.success){
+            setJobs(data.jobs)
+        }else{
+            toast.error(data.message)
+        }
+    } catch (error) {
+        toast.error(error.message)
+    }
+    
    }
+
+   //func to fetch company data 
+   const fetchCompanyData = async () => {
+
+    try {
+        
+        const {data} = await axios.get(backendUrl + "/api/company/company", {headers : {token : companyToken}})
+        
+        if(data.success){
+            setCompanyData(data.company)
+            
+        }else{
+            toast.error(data.message)
+        }
+
+    } catch (error) {
+        toast.error(error.message)           
+    }
+
+   }
+
+   //to fetch user data
+   const fetchUserData = async()=>{
+    try {
+        const token = await getToken()
+        const {data}= await axios.get(backendUrl + "/api/users/user", {headers: {Authorization: `Bearer ${token}`}})
+        if(data.success){
+            setUserData(data.user)
+
+        }else{
+            toast.error(data.message)
+        }
+    } catch (error) {
+        toast.error(error.message)
+    }
+   }
+
+   //fetch user apply application data
+
+   const fetchUserApplications = async()=>{
+    try {
+        const token = await getToken()
+        const {data} = await axios.get(backendUrl + "/api/users/applications",
+            {headers: {Authorization: `Bearer ${token}`}}
+        )
+        if(data.success){
+            setUserApplications(data.applications)
+
+        }else{
+            toast.error(data.message)
+        }
+    } catch (error) {
+        toast.error(error.message)
+    }
+   }
+
 
    useEffect(() => {
      fetchJobs()
+
+     const storedCompanyToken = localStorage.getItem("companyToken")
+     if(storedCompanyToken){
+        setCompanyToken(storedCompanyToken)
+     }
    }, [])
+   
+   useEffect(() => {
+    if(companyToken){
+        fetchCompanyData()
+    }
+   }, [companyToken])
+
+   useEffect(() => {
+     if(user){
+        fetchUserData()
+        fetchUserApplications()
+     }
+   }, [user])
+   
    
    
     const value = {
         setSearchFilter, searchFilter,
         isSearch, setIsSearch,
         jobs, setJobs,
-        setShowRecruiterLogin, showRecruiterLogin
+        setShowRecruiterLogin, showRecruiterLogin,
+        companyToken, setCompanyToken,
+        companyData, setCompanyData,
+        backendUrl, 
+        userData, setUserData,
+        userApplications, setUserApplications,
+        fetchUserData, 
+        fetchUserApplications
     }
     return(<AppContext.Provider value={value} >
         {props.children}
